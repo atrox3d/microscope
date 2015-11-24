bed6a96b27fdd0f0c77527bc912c4534592f633f
atrox3d <atrox3d@gmail.com>
2015-11-24 17:21:47 +0100

    Add posts collection

--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -1,25 +1,6 @@
-var postData = [
-	{
-      title: 'Introducing Telescope',
-      author: 'Sacha Greif',
-      url: 'http://sachagreif.com/introducing-telescope/'
-    },
-    {
-      title: 'Meteor',
-      author: 'Tom Coleman',
-      url: 'http://meteor.com'
-    },
-    {
-      title: 'The Meteor Book',
-      author: 'Tom Coleman',
-      url: 'http://themeteorbook.com'
-    }
-];
-
-Template.postsList.helpers(
-	{
-		// funziona anche come return di funzione
-		//posts: function() {return postData;}
-		posts: postData
+Template.postsList.helpers({
+		posts: function () {
+			return Posts.find();
+		}
 	}
 );
