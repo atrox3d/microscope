59768d810cdba5dcb62787f3f3a8a5fdf2985fcc
atrox3d <atrox3d@gmail.com>
2015-11-26 10:36:57 +0100

    Update security with Methods

--- collections/security.js
+++ collections/security.js
@@ -1,10 +1,20 @@
 Security = new Meteor.Collection('security');
 
-Security.allow({
-	insert: function(userId, doc) {
-		return !!userId;
+// Security.allow({
+// 	insert: function(userId, doc) {
+// 		return !!userId;
+// 	},
+// 	remove: function(userId, doc) {
+// 		return !!userId;
+// 	}
+// });
+
+Meteor.methods({
+	security_insert: function() {
+		var securityId = Security.insert({test:'test'});
+		return securityId;
 	},
-	remove: function(userId, doc) {
-		return !!userId;
+	security_remove: function(id) {
+		Security.remove(id);
 	}
 });
